17e527c5288a4af26a68693b3bd9f8a696e45c72
closedwall <yadavrajesh5612@gmail.com>
2023-12-16 21:55:36 +0530

    formatted string using function

---
+++ day10/day10_start.py
@@ -0,0 +1,9 @@
+print("Welcome to day 10")
+
+def format_name(f_name,l_name):
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    return f"{formatted_f_name} {formatted_l_name}"
+
+formatted_name = format_name("RaJESh","yaDav")
+print(formatted_name)
