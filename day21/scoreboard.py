6260762a095c1c0f8c180816f1669c37a3729a8b
closedwall <yadavrajesh5612@gmail.com>
2024-01-17 19:55:45 +0530

    complete day21 with and wrapped up snake game

---
+++ day21/scoreboard.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+ALIGNMENT="center"
+FONT = ("Arial", 20,"normal")
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score=0
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(0,270)
+        self.update_score()
+    
+    def game_over(self):
+        self.goto(0,0)
+        self.write("Game Over", align=ALIGNMENT, font=FONT)
+
+    def update_score(self):
+        self.clear()
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.score+=1
